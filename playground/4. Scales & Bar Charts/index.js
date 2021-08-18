5947b32a4a9f7b3934d21e130d22070e47b7c75f
solygambas <51904909+solygambas@users.noreply.github.com>
2021-08-18 07:26:59 +0200

    add linear scales

---
+++ playground/4. Scales & Bar Charts/index.js
@@ -0,0 +1,24 @@
+const svg = d3.select("svg");
+
+d3.json("menu.json").then((data) => {
+  // linear scale
+  const y = d3.scaleLinear().domain([0, 1000]).range([0, 500]);
+
+  // join the data to rects
+  const rects = svg.selectAll("rect").data(data);
+
+  rects
+    .attr("width", 50)
+    .attr("height", (d) => y(d.orders))
+    .attr("fill", "orange")
+    .attr("x", (d, i) => i * 70);
+
+  // append the enter selection to the DOM
+  rects
+    .enter()
+    .append("rect")
+    .attr("width", 50)
+    .attr("height", (d) => y(d.orders))
+    .attr("fill", "orange")
+    .attr("x", (d, i) => i * 70);
+});
