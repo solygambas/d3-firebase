4df5cd2b0a00084bc774588f41beae804ba999d7
solygambas <51904909+solygambas@users.noreply.github.com>
2021-08-28 14:17:37 +0200

    add times and axes

--- line-graphs/README.md
+++ line-graphs/README.md
@@ -10,5 +10,6 @@ A fitness tracker displaying line graphs.
 
 - creating an HTML template with Materialize.
 - handling DOM events and adding data to Firestore.
+- formatting times and axes with D3.
 
 Based on [Build Data Visualizations with D3.js & Firebase](https://www.udemy.com/course/build-data-uis-with-d3-firebase/) by Shaun Pelling - The Net Ninja (2019)
---
+++ line-graphs/graph.js
@@ -0,0 +1,73 @@
+const margin = { top: 40, right: 20, bottom: 50, left: 100 };
+const graphWidth = 560 - margin.left - margin.right;
+const graphHeight = 400 - margin.top - margin.bottom;
+
+const svg = d3
+  .select(".canvas")
+  .append("svg")
+  .attr("width", graphWidth + margin.left + margin.right)
+  .attr("height", graphHeight + margin.top + margin.bottom);
+
+const graph = svg
+  .append("g")
+  .attr("width", graphWidth)
+  .attr("height", graphHeight)
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// scales
+const x = d3.scaleTime().range([0, graphWidth]);
+const y = d3.scaleLinear().range([graphHeight, 0]);
+
+// axes groups
+const xAxisGroup = graph
+  .append("g")
+  .attr("class", "x-axis")
+  .attr("transform", `translate(0, ${graphHeight})`);
+const yAxisGroup = graph.append("g").attr("class", "y-axis");
+
+const update = (data) => {
+  // set scale domains
+  x.domain(d3.extent(data, (data) => new Date(data.date)));
+  y.domain([0, d3.max(data, (data) => data.distance)]);
+
+  // create axes
+  const xAxis = d3.axisBottom(x).ticks(4).tickFormat(d3.timeFormat("%b %d"));
+  const yAxis = d3
+    .axisLeft(y)
+    .ticks(4)
+    .tickFormat((data) => data + "m");
+
+  // call axes
+  xAxisGroup.call(xAxis);
+  yAxisGroup.call(yAxis);
+
+  // rotate axis text
+  xAxisGroup
+    .selectAll("text")
+    .attr("text-anchor", "end")
+    .attr("transform", "rotate(-40)");
+};
+
+// data and firestore
+let data = [];
+
+db.collection("activities").onSnapshot((res) => {
+  res.docChanges().forEach((change) => {
+    const doc = { ...change.doc.data(), id: change.doc.id };
+    switch (change.type) {
+      case "added":
+        data.push(doc);
+        break;
+      case "modified":
+        const index = data.findIndex((item) => item.id === doc.id);
+        data[index] = doc;
+        break;
+      case "removed":
+        data = data.filter((item) => item.id !== doc.id);
+        break;
+      default:
+        break;
+    }
+  });
+  update(data);
+});
