5ff22951feb23c524a1c87f9c39d417b74d5fcd7
solygambas <51904909+solygambas@users.noreply.github.com>
2021-08-28 13:30:16 +0200

    add handling DOM events and adding data to Firestore

--- line-graphs/README.md
+++ line-graphs/README.md
@@ -9,5 +9,6 @@ A fitness tracker displaying line graphs.
 ## Features
 
 - creating an HTML template with Materialize.
+- handling DOM events and adding data to Firestore.
 
 Based on [Build Data Visualizations with D3.js & Firebase](https://www.udemy.com/course/build-data-uis-with-d3-firebase/) by Shaun Pelling - The Net Ninja (2019)
---
+++ line-graphs/index.js
@@ -0,0 +1,42 @@
+const buttons = document.querySelectorAll("button");
+const form = document.querySelector("form");
+const formActivity = document.querySelector("form span");
+const input = document.querySelector("input");
+const error = document.querySelector(".error");
+let activity = "cycling";
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    // get activity
+    activity = e.target.dataset.activity;
+
+    // remove and add active class
+    buttons.forEach((button) => button.classList.remove("active"));
+    e.target.classList.add("active");
+
+    // set ID of input field
+    input.setAttribute("id", activity);
+
+    // set text of form span
+    formActivity.textContent = activity;
+  });
+});
+
+// form submit
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const distance = parseInt(input.value);
+  if (distance) {
+    error.textContent = "";
+    const res = await db.collection("activities").add({
+      distance,
+      activity,
+      date: new Date().toString(),
+    });
+    if (res) {
+      input.value = "";
+    }
+  } else {
+    error.textContent = "Please enter a valid distance";
+  }
+});
